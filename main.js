9e6c5fefd9ba6bef515dae55793d4554fae1575c
Kelly H <kellyaccnts@gmail.com>
2019-10-20 11:41:17 -0700

    Various visual and implementation enhancements.

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-//import Tone from "./node_modules/tone/Tone";
+// init
+
+document.querySelector("body").addEventListener("click", (e)=>{
+    if (!e.target.classList.contains("seq-switch")) {
+        return false;
+    }
+    if (e.target.classList.contains("switch-active")){
+        e.target.classList.remove("switch-active");
+    }
+    else { e.target.classList.add("switch-active"); }
+})
+
+// setup tone
 
 var synth = new Tone.MembraneSynth().toMaster()
+//Tone.Transport.latencyHint = 'fastest';
 var division = 1;
 
 //create a loop
@@ -12,14 +25,14 @@ var loop = new Tone.Loop(function(time){
     Tone.Draw.schedule(function(time) {
         //the callback synced to the animation frame
         const column = document.querySelector("#sq1-" + division);
-        column.classList.add("active");
+        column.classList.add("col-active");
         setTimeout(() => {
-            column.classList.remove("active");
+            column.classList.remove("col-active");
         }, 100);
     });
 
 }, "16n")
 
 //play the loop between 0-2m on the transport
-loop.start(0.1);
+loop.start(0);
 document.querySelector('#tone-toggle').addEventListener('click', e =>  { Tone.Transport.toggle()})
