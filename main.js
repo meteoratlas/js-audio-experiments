d1a29050d085f37c6903c46aba683f948cedffb4
Kelly H <kellyaccnts@gmail.com>
2019-10-19 23:23:28 -0700

    Audio engine integration

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("entry point");
+//import Tone from "./node_modules/tone/Tone";
+
+var synth = new Tone.MembraneSynth().toMaster()
+
+//create a loop
+var loop = new Tone.Loop(function(time){
+	synth.triggerAttackRelease("C1", "8n", time)
+}, "4n")
+
+//play the loop between 0-2m on the transport
+loop.start(0).stop('2m')
+document.querySelector('#tone-toggle').addEventListener('click', e =>  { Tone.Transport.toggle()})
