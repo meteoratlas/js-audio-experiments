5069764b2122690fe936612bf4f5d47b887d4a11
Kelly H <kellyaccnts@gmail.com>
2019-10-20 00:00:48 -0700

    Basic 16th loop example added

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 //import Tone from "./node_modules/tone/Tone";
 
 var synth = new Tone.MembraneSynth().toMaster()
+var division = 1;
 
 //create a loop
 var loop = new Tone.Loop(function(time){
-	synth.triggerAttackRelease("C1", "8n", time)
-}, "4n")
+    synth.triggerAttackRelease("C4", "8n", time)
+    division %= 16;
+    division++;
+
+    Tone.Draw.schedule(function(time) {
+        //the callback synced to the animation frame
+        const column = document.querySelector("#sq1-" + division);
+        column.classList.add("active");
+        setTimeout(() => {
+            column.classList.remove("active");
+        }, 100);
+    });
+
+}, "16n")
 
 //play the loop between 0-2m on the transport
-loop.start(0).stop('2m')
+loop.start(0.1);
 document.querySelector('#tone-toggle').addEventListener('click', e =>  { Tone.Transport.toggle()})
